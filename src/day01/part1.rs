777846402e16b226fd49ec0fce3280e7ee06e6a4
Greg Sleap <greg.sleap@curtin.edu.au>
2021-12-07 16:53:26 +0800

    day01

---
+++ src/day01/part1.rs
@@ -0,0 +1,33 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+fn main() {
+    let filename = "src/day01/input.txt";
+
+    let data = read_iter(filename);
+
+    let mut increases = 0;
+
+    for i in 1..data.len() {
+        if data[i] - data[i - 1] > 0 {
+            increases += 1;
+        }
+    }
+
+    println!("{} increases\n", increases);
+}
+
+// Read file into vector
+fn read_iter(filename: &str) -> Vec<i32> {
+    let file = File::open(filename).expect("file not found!");
+    let reader = BufReader::new(file);
+    let mut vec: Vec<i32> = vec![];
+
+    for line in reader.lines() {
+        let val: i32 = line.expect("Error").parse().expect("error parsing");
+        vec.push(val);
+    }
+
+    vec
+}
